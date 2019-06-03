74a1b4e2fe339c430c326265256ffde84b36c7fd
myriam mansouri <myriam.mansouri@gmail.com>
2019-06-03 12:22:02 -0400

    first commit

---
+++ build/precache-manifest.8a387eec16af2a85e0c09f7067b6b419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44db8e00c52acef23abba107d2821c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf4501383234bf5ffc9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a95dc0035e1ab7f9f276",
+    "url": "/static/css/main.a2e6583f.chunk.css"
+  },
+  {
+    "revision": "cbf4501383234bf5ffc9",
+    "url": "/static/js/2.23b1bc47.chunk.js"
+  },
+  {
+    "revision": "a95dc0035e1ab7f9f276",
+    "url": "/static/js/main.d8e915bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
